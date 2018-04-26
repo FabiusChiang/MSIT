d54f8a84a713e25d7db9c042e48cda13a3a517dd
fjiang4 <fabius.jiang@morningstar.com>
2018-04-26 18:39:37 -0500

    init project1

--- datamining/project1/p1.R
+++ datamining/project1/p1.R
@@ -1,1 +1,13 @@
+library(car);
+
 purchase=read.csv("party.csv")
+
+names(purchase);
+# plot(purchase);
+# purchase;
+model1=lm(mile1 ~ particpate+elaboration+prefood, purchase);
+
+vif(lm)
+# lm()
+
+# plot()
