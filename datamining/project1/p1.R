9c7aab9e4dbf5498948356b1d572821f1700c8fe
fjiang4 <fabius.jiang@morningstar.com>
2018-04-26 19:39:49 -0500

    get a better solution

--- datamining/project1/p1.R
+++ datamining/project1/p1.R
@@ -1,13 +1,20 @@
 library(car);
 
 purchase=read.csv("party.csv")
+head(purchase,4)
+attach(purchase)
 
-names(purchase);
-# plot(purchase);
-# purchase;
-model1=lm(mile1 ~ particpate+elaboration+prefood, purchase);
 
-vif(lm)
-# lm()
+# model1=lm(mile1 ~ as.factor(particpate)+elaboration+prefood+Rfreq, purchase);
+# summary(model1);
+# plot(model1);
 
-# plot()
+purchase$postTotal = mile1 + mile2 + mile3 + mile4;
+# model2=lm(postTotal ~ as.factor(particpate)+elaboration+Rfreq+basemile, purchase);
+# summary(model2);
+# plot(model2);
+
+purchase$diffRatio = (purchase$postTotal - basemile)/basemile;
+model3=lm(log(diffRatio) ~ as.factor(particpate)+elaboration+Rfreq+log(basemile), purchase);
+summary(model3);
+plot(model3);
